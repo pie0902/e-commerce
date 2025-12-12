991ed64b72fccf7a380c2ec98d0cfbf2b050680b
thunder <thunder@thunderui-Macmini.local>
2025-12-12 15:54:37 +0900

    config

---
+++ dollar-user/src/main/java/org/example/dollaruser/global/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    @Order(0)
+    public SecurityFilterChain appSecurity(
+            HttpSecurity http,
+            JwtAuthorizationFilter jwtAuthorizationFilter
+    ) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/users/signup", "/users/login", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
