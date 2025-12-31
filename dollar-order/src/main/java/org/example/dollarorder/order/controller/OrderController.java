62e0072344cf4f284b962552155576dc322ac96f
pie0902 <zhfoqkq@gmail.com>
2025-12-31 15:24:30 +0900

    Add GitHub Actions CI/deploy and stabilize docker jar copy

--- dollar-order/src/main/java/org/example/dollarorder/order/controller/OrderController.java
+++ dollar-order/src/main/java/org/example/dollarorder/order/controller/OrderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;=
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -67,4 +67,3 @@ public class OrderController {
 
 
 }
-
